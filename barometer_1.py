23f7da9ce5f58d4fe32717b3c081f4885cb7aecd
Jens U. Moeller <jensumoeller@me.com>
2023-08-04 00:09:32 +0200

    Änderung der Maßeinheiten

--- barometer_1.py
+++ barometer_1.py
@@ -5,7 +5,7 @@ import Adafruit_BMP.BMP085 as BMP085
 sensor = BMP085.BMP085()
 
 print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
-print('QFE = {0:0.2f} Pa'.format(sensor.read_pressure()/100))
+print('QFE = {0:0.2f} hPa'.format(sensor.read_pressure()/100))
 print('Alt = {0:0.2f} m'.format(sensor.read_altitude()))
-print('QNH = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()/100))
+print('QNH = {0:0.2f} hPa'.format(sensor.read_sealevel_pressure()/100))
 
