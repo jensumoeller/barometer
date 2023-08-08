ee9d640089ce843482fe193aeacfd23d0f66fa1c
Jens U. Moeller <jensumoeller@me.com>
2023-08-08 21:08:51 +0200

    Ausgabe Luftdruck über OLED

--- barometer_3.py
+++ barometer_3.py
@@ -3,13 +3,17 @@ import numpy as np
 import board
 import RPi.GPIO as GPIO
 import Adafruit_BMP.BMP085 as BMP085
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import sh1106, ssd1306
+from PIL import ImageFont, ImageDraw, Image
 
 # Setze den GPIO-Modus auf BCM für die Ansteuerung der LED
 GPIO.setmode(GPIO.BCM)
 
 # Definiere die GPIO-Pins für die LEDs
-led_grn = 4
-led_red = 17
+led_grn = 20
+led_red = 21
 
 # Initialisiere die GPIO-Pins als Ausgänge
 GPIO.setup(led_red, GPIO.OUT)
@@ -18,12 +22,17 @@ GPIO.setup(led_grn, GPIO.OUT)
 # Initialisiere den Adafruit BMP185 Luftdrucksensor
 sensor = BMP085.BMP085()
 
+# Initialisieren des OLED Displays
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial, height=32, width=128, rotate=0)
+
+
 # Ermitteln des durchschnittlichen QFE aus 10 Messungen
 def qfe_mittel():
     luftdruck = []        #Array initialisieren
     for i in range(10):
         luftdruck.append(sensor.read_pressure())
-        time.sleep(10)    #10 Sekunden warten bis zur nächsten Messung
+        time.sleep(1)    #1 Sekunde warten bis zur nächsten Messung
         
     return np.mean(luftdruck)/100
 
@@ -34,7 +43,7 @@ def temperatur():
     return temperatur
 
 # Schalten der LED auf rot oder grün
-def anzeige(farbe):
+def led_anzeige(farbe):
     if farbe == 'rot':
         GPIO.output(led_red, GPIO.HIGH)
         GPIO.output(led_grn, GPIO.LOW)
@@ -52,34 +61,47 @@ def anzeige(farbe):
 # ====== HAUPTPROGRAMM =======
 def main():
 
-    # Initialisieren der Variable für aktuellen Luftdruck
+    # Schriftart für das OLED-Display setzen
+    oled_font = ImageFont.truetype('FreeSans.ttf', 14)
+
+    # Iitialisieren der Variable für aktuellen Luftdruck
     qfe_neu = 1013.25     # Initialer Luftdruck in hPa für Vergleich
     delta = 0.3           # minimaler Differenzdruck für Trend
     intervall = 30        # Intervalldauer der Messungen in Minuten
-    anzeige('beide')      # beide LED initial an
+    led_anzeige('beide')  # beide LED initial an
 
     while True:
-        anzeige('')                     # LED aus bei Ermittlung
+        led_anzeige('')                 # LED aus bei Ermittlung
+        with canvas(device) as draw:
+            # draw.rectangle(device.bounding_box, outline = "white", fill = "black")
+            draw.text((2, 2), "Messung ...", font = oled_font, fill = "white")
+
         qfe_alt = qfe_neu
         qfe_neu = qfe_mittel()
 
         if qfe_neu - qfe_alt > delta:   # Luftdruck steigt
-            anzeige('gruen')
+            led_anzeige('gruen')
         elif qfe_alt - qfe_neu > delta: # Luftdruck fällt
-            anzeige('rot')
+            led_anzeige('rot')
         else:                           # Luftdruck konstant
-            anzeige('beide')
+            led_anzeige('beide')
         
-        print('QFE = {0:0.1f} hPa'.format(qfe_mittel()))
-        print('Temp = {0:0.1f} *C'.format(temperatur()))
+        # print('QFE = {0:0.1f} hPa'.format(qfe_mittel()))
+        # print('Temp = {0:0.1f} *C'.format(temperatur()))
         # print('Alt = {0:0.2f} m'.format(sensor.read_altitude()))
         # print('QNH = {0:0.2f} hPa'.format(sensor.read_sealevel_pressure()/100))
 
-        # Ausgabe der aktuellen Systemzeit in UTC
-        aktuelle_zeit_utc = time.gmtime(time.time())
-        print(time.strftime('%d %H:%M', aktuelle_zeit_utc))
+        # Ermittlung und Format der UTC-Zeit und des Luftdrucks
+        aktuelle_zeit_utc = time.strftime('%H:%M', time.gmtime(time.time()))
+        qfe_formatiert = "{:.1f}".format(qfe_neu)
+        qfe_delta_formatiert = "{:.1f}".format(qfe_neu - qfe_alt)
 
- 
+        # Ausgabe über OLED Display
+        oled_ausgabe = 'QFE ' + qfe_formatiert + '  (' + qfe_delta_formatiert + ')'
+        with canvas(device) as draw:
+            draw.text((0, 0), oled_ausgabe, font = oled_font, fill = "white")
+            draw.text((0,18), aktuelle_zeit_utc +'Z', font = oled_font, fill = "white")
+
         time.sleep(intervall * 60)
         
 
